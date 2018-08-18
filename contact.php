bf345a1328e2f9dc07e2f1048f6019121f171f02
Tyler <42299954+FunkyMonkster@users.noreply.github.com>
2018-08-18 13:23:51 -0400

    blah

--- contact.php
+++ contact.php
@@ -3,7 +3,8 @@
 <html>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8">
-    <title>Contact Me: Tyler Westmoreland</title>
+    <link rel="icon" href="images/logo.png" type="image/gif"/>
+    <title>Contact Me!</title>
     <link rel="stylesheet" href="https://use.typekit.net/upa8vfn.css">
     <link href="css/grid-flex-style.css" type="text/css" rel="stylesheet"/>
     <link href="css/main-style.css" type="text/css" rel="stylesheet"/>
@@ -16,7 +17,7 @@
         <a href="index.html#about">About</a>
         <a href="gallery.html">Gallery</a>
         <a href="index.html#skills">Skills</a>
-        <a href="contact.html">Contact</a>
+        <a href="contact.php">Contact</a>
       </div>
       <div class="cl-1"></div>
     </div>
@@ -28,7 +29,26 @@
         <p>Please include the deadline of the project and a brief description of what it is. I will get back to you as soon as possible. If you have some advice on how to be an adult, I&#39;d appreciate that too.</p>
       </div>
 
-      <form class="cl-3 cl" method="post" action="index.php">
+      <?php
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $from = 'From: Westmoreland Design';
+        $to = 'sendform@westmorelandt.com';
+        $subject = 'New Client';
+
+        $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
+        if ($_POST['submit']) {
+          if (mail ($to, $subject, $body, $from)) {
+            echo "<p>Your message has been sent!</p>";
+          } else {
+            echo "<p>Something went wrong, please go back and try again!</p>";
+          }
+        }
+      ?>
+
+      <form class="cl-3 cl" method="post" action="contact.php">
         <input name="name" type="text" placeholder="Your Name"/> <br>
         <input name="email" type="email" placeholder="youremail@gmail.com"/> <br>
         <textarea name="message" placeholder="Your message"></textarea>
